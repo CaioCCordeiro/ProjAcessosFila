90b93ade7546332c41dcdfc96e0ebabfbd697767
Caio Costa Cordeiro <caio.c.cordeiro@outlook.com>
2019-11-14 19:34:08 -0300

    Finalização dos Métodos do Menu

--- projAcessosFila/Program.cs
+++ projAcessosFila/Program.cs
@@ -58,21 +58,25 @@ namespace projAcessosFila
             int id;
             string nome;
 
+            Console.WriteLine("--------CADASTRAR AMBIENTE--------");
             Console.Write("ID do Ambiente: ");
             id = int.Parse(Console.ReadLine());
             Console.Write("Nome: ");
             nome = Console.ReadLine();
 
             this.cad.adicionarAmbiente(new Ambiente(id, nome));
-            
+
+            Console.WriteLine("----------------");
         }
 
         public void consultarAmbiente()
         {
             int id;
 
+            Console.WriteLine("--------CONSULTAR AMBIENTE--------");
             Console.Write("ID do Ambiente: ");
             id = int.Parse(Console.ReadLine());
+            Console.WriteLine("----------------");
 
             Ambiente pesquisado = this.cad.pesquisarAmbiente(new Ambiente(id));
 
@@ -80,16 +84,19 @@ namespace projAcessosFila
                 Console.WriteLine("Não foi possível encontrar o ambiente pesquisado");
             else
             {
-                Console.WriteLine("ID do Ambiente: " + pesquisado.Id);
+                Console.WriteLine("ID: " + pesquisado.Id);
                 Console.WriteLine("Nome: " + pesquisado.Nome);
                 Console.WriteLine("Número de Logs: " + pesquisado.Logs.Count());
             }
+
+            Console.WriteLine("----------------");
         }
 
         public void excluirAmbiente()
         {
             int id;
 
+            Console.WriteLine("--------EXCLUIR AMBIENTE--------");
             Console.Write("ID do Ambiente: ");
             id = int.Parse(Console.ReadLine());
 
@@ -97,6 +104,8 @@ namespace projAcessosFila
                 Console.WriteLine("Ambiente excluído com sucesso!");
             else
                 Console.WriteLine("Erro ao excluir o ambiente");
+
+            Console.WriteLine("----------------");
         }
 
         public void cadastrarUsuario()
@@ -104,20 +113,25 @@ namespace projAcessosFila
             int id;
             string nome;
 
+            Console.WriteLine("--------CADASTRAR USUÁRIO--------");
             Console.Write("ID do Usuário: ");
             id = int.Parse(Console.ReadLine());
             Console.Write("Nome: ");
             nome = Console.ReadLine();
 
             this.cad.adicionarUsuario(new Usuario(id, nome));
+
+            Console.WriteLine("----------------");
         }
 
         public void consultarUsuario()
         {
             int id;
 
+            Console.WriteLine("--------CONSULTAR USUÁRIO--------");
             Console.Write("ID do Usuário: ");
             id = int.Parse(Console.ReadLine());
+            Console.WriteLine("----------------");
 
             Usuario pesquisado = this.cad.pesquisarUsuario(new Usuario(id));
 
@@ -125,16 +139,19 @@ namespace projAcessosFila
                 Console.WriteLine("Não foi possível encontrar o usuário pesquisado");
             else
             {
-                Console.WriteLine("ID do Usuário: " + pesquisado.Id);
+                Console.WriteLine("ID: " + pesquisado.Id);
                 Console.WriteLine("Nome: " + pesquisado.Nome);
                 Console.WriteLine("Número de Ambientes: " + pesquisado.Ambientes.Count());
             }
+
+            Console.WriteLine("----------------");
         }
 
         public void excluirUsuario()
         {
             int id;
 
+            Console.WriteLine("--------EXCLUIR USUÁRIO--------");
             Console.Write("ID do Usuário: ");
             id = int.Parse(Console.ReadLine());
 
@@ -142,12 +159,15 @@ namespace projAcessosFila
                 Console.WriteLine("Usuário excluído com sucesso!");
             else
                 Console.WriteLine("Erro ao excluir o usuário");
+
+            Console.WriteLine("----------------");
         }
 
         public void permitirAcesso()
         {
             int idUsuario, idAmbiente;
 
+            Console.WriteLine("--------PERMITIR ACESSO--------");
             Console.Write("ID do Ambiente: ");
             idAmbiente = int.Parse(Console.ReadLine());
 
@@ -172,7 +192,11 @@ namespace projAcessosFila
                         {
                             if (u == paraU)
                             {
-                                u.concederPermissao(paraA);
+                                if (u.concederPermissao(paraA))
+                                    Console.WriteLine("Permissão concedida com sucesso!");
+                                else
+                                    Console.WriteLine("ERRO! Permissão já concedida");
+
                                 break;
                             }
                         }
@@ -181,21 +205,154 @@ namespace projAcessosFila
                 } while (this.cad.pesquisarUsuario(new Usuario(idUsuario)).Id == 0);
             }
 
+            Console.WriteLine("----------------");
         }
 
         public void revogarAcesso()
         {
+            int idUsuario, idAmbiente;
 
+            Console.WriteLine("--------REVOGAR ACESSO--------");
+            Console.Write("ID do Ambiente: ");
+            idAmbiente = int.Parse(Console.ReadLine());
+
+            if (this.cad.pesquisarAmbiente(new Ambiente(idAmbiente)).Id == 0)
+                Console.WriteLine("Não foi possível encontrar o ambiente pesquisado");
+            else
+            {
+                Ambiente paraA = this.cad.pesquisarAmbiente(new Ambiente(idAmbiente));
+
+                do
+                {
+                    Console.Write("ID do Usuário: ");
+                    idUsuario = int.Parse(Console.ReadLine());
+
+                    if (this.cad.pesquisarUsuario(new Usuario(idUsuario)).Id == 0)
+                        Console.WriteLine("Não foi possível encontrar o usuário pesquisado");
+                    else
+                    {
+                        Usuario paraU = this.cad.pesquisarUsuario(new Usuario(idUsuario));
+
+                        foreach (Usuario u in this.cad.Usuarios)
+                        {
+                            if (u == paraU)
+                            {
+                                if (u.revogarPermissao(paraA))
+                                    Console.WriteLine("Permissão revogada com sucesso!");
+                                else
+                                    Console.WriteLine("Esta permissão já não estava concedida");
+
+                                break;
+                            }
+                        }
+                    }
+
+                } while (this.cad.pesquisarUsuario(new Usuario(idUsuario)).Id == 0);
+            }
+
+            Console.WriteLine("----------------");
         }
 
         public void registrarAcesso()
         {
+            int idUsuario, idAmbiente;
 
+            Console.WriteLine("--------REGISTRAR ACESSO--------");
+            Console.Write("ID do Ambiente: ");
+            idAmbiente = int.Parse(Console.ReadLine());
+
+            if (this.cad.pesquisarAmbiente(new Ambiente(idAmbiente)).Id == 0)
+                Console.WriteLine("Não foi possível encontrar o ambiente pesquisado");
+            else
+            {
+                Ambiente paraA = this.cad.pesquisarAmbiente(new Ambiente(idAmbiente));
+
+                do
+                {
+                    Console.Write("ID do Usuário: ");
+                    idUsuario = int.Parse(Console.ReadLine());
+
+                    if (this.cad.pesquisarUsuario(new Usuario(idUsuario)).Id == 0)
+                        Console.WriteLine("Não foi possível encontrar o usuário pesquisado");
+                    else
+                    {
+                        Usuario paraU = this.cad.pesquisarUsuario(new Usuario(idUsuario));
+
+                        bool acesso;
+                        if (paraU.Ambientes.Contains(paraA))
+                            acesso = true;
+                        else
+                            acesso = false;
+
+                        Log log = new Log(DateTime.Now, paraU, acesso);
+
+                        foreach (Ambiente a in this.cad.Ambientes)
+                        {
+                            if (a == paraA)
+                            {
+                                a.registrarLog(log);
+                                break;
+                            }
+                        }
+                    }
+
+                } while (this.cad.pesquisarUsuario(new Usuario(idUsuario)).Id == 0);
+            }
+
+            Console.WriteLine("----------------");
         }
 
         public void consultarLogs()
         {
+            int id, opcao;
 
+            Console.WriteLine("--------CONSULTAR LOGS--------");
+            Console.Write("ID do Ambiente: ");
+            id = int.Parse(Console.ReadLine());
+
+            if (this.cad.pesquisarAmbiente(new Ambiente(id)).Id == 0)
+                Console.WriteLine("Não foi possível encontrar o ambiente pesquisado");
+            else
+            {
+                Console.WriteLine("Filtrar por: ");
+                Console.WriteLine("0. Todos / 1. Autorizados / 2. Negados");
+                Console.Write("Opção: ");
+                opcao = int.Parse(Console.ReadLine());
+                Console.WriteLine("----------------");
+
+                Ambiente pesquisado = this.cad.pesquisarAmbiente(new Ambiente(id));
+
+
+                if(opcao == 1)
+                    foreach (Log l in pesquisado.Logs)
+                    {
+                        if (l.TipoAcesso)
+                        {
+                            Console.WriteLine("Data: " + l.DtAcesso);
+                            Console.WriteLine("Usuário: " + l.Usuario.Nome);
+                        }
+                    }
+                else if(opcao == 2)
+                    foreach (Log l in pesquisado.Logs)
+                    {
+                        if (!l.TipoAcesso)
+                        {
+                            Console.WriteLine("Data: " + l.DtAcesso);
+                            Console.WriteLine("Usuário: " + l.Usuario.Nome);
+                        }
+                    }
+                else
+                    foreach (Log l in pesquisado.Logs)
+                    {
+                        Console.WriteLine("Data: " + l.DtAcesso);
+                        Console.WriteLine("Usuário: " + l.Usuario.Nome);
+                        Console.Write("Tipo de Acesso: ");
+                        if (l.TipoAcesso)
+                            Console.WriteLine("Autorizado");
+                        else
+                            Console.WriteLine("Negado");
+                    }
+            }
         }
     }
 }
